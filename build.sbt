f64c5c6c97a906cdef90573a1fb5154a96275c0a
Hsueh-Min Chang <hsuehmin.chang@linecorp.com>
2022-04-11 15:45:49 +0800

    Remove duplicate implicits

--- build.sbt
+++ build.sbt
@@ -2,15 +2,15 @@ name := "kafka-streams-schema-helper"
 organization := "tw.idv.idiotech"
 version := "0.0.1"
 
-scalaVersion := "2.12.10"
+scalaVersion := "2.12.13"
 
 val thirdPartyRepos = Seq(
   "confluent-release" at "https://packages.confluent.io/maven/"
 )
 resolvers := (thirdPartyRepos ++ resolvers.value)
 
-val confluentAvroVersion = "5.4.2"
-val kafkaVersion = "2.4.1"
+val confluentAvroVersion = "6.2.0"
+val kafkaVersion = "2.8.0"
 
 val testDependencies = Seq(
   "org.apache.kafka" % "kafka-streams-test-utils" % kafkaVersion,
@@ -21,7 +21,7 @@ libraryDependencies ++= Seq(
   "io.confluent" % "kafka-streams-avro-serde" % confluentAvroVersion,
   "org.apache.kafka" %% "kafka-streams-scala" % kafkaVersion,
   "org.apache.kafka" % "kafka-clients" % kafkaVersion,
-  "com.sksamuel.avro4s" %% "avro4s-core" % "4.0.0",
+  "com.sksamuel.avro4s" %% "avro4s-core" % "4.0.10",
   "javax.ws.rs" % "javax.ws.rs-api" % "2.1.1" artifacts Artifact(
     "javax.ws.rs-api",
     "jar",
--- src/main/scala/tw/idv/idiotech/kafkastreams/avro/StreamsImplicits.scala
+++ src/main/scala/tw/idv/idiotech/kafkastreams/avro/StreamsImplicits.scala
@@ -1,31 +1,32 @@
 package tw.idv.idiotech.kafkastreams.avro
 
 import org.apache.kafka.streams.processor.StateStore
-import org.apache.kafka.streams.scala.kstream.{ Consumed, Grouped, Joined, Materialized, Produced }
-import org.apache.kafka.common.serialization.{ Deserializer, Serde, Serializer, Serdes => JSerdes }
+import org.apache.kafka.streams.scala.kstream.{Consumed, Grouped, Joined, Materialized, Produced}
+import org.apache.kafka.common.serialization.{Deserializer, Serde, Serializer, Serdes => JSerdes}
+import org.apache.kafka.common.utils.Bytes
+import org.apache.kafka.streams.state.KeyValueStore
 
 trait StreamsImplicits {
   import StreamsImplicits._
-  implicit def numberSerde[V <: AnyVal](implicit serde: Serde[V]) = new KeyValueSerde[V] {
-    override def serializer(): Serializer[V] = serde.serializer()
-    override def deserializer(): Deserializer[V] = serde.deserializer()
-  }
 
-  implicit val stringSerde = new KeyValueSerde[String] {
-    override def serializer(): Serializer[String] = JSerdes.String().serializer()
-    override def deserializer(): Deserializer[String] = JSerdes.String().deserializer()
+  implicit def stringSerde: KeyValueSerde[String] = JSerdes.String().toKeyValueSerde()
+  implicit def longSerde: KeyValueSerde[Long] = JSerdes.Long().asInstanceOf[Serde[Long]].toKeyValueSerde()
+  implicit def javaLongSerde: KeyValueSerde[java.lang.Long] = JSerdes.Long().toKeyValueSerde()
+  implicit def byteArraySerde: KeyValueSerde[Array[Byte]] = JSerdes.ByteArray().toKeyValueSerde()
+  implicit def bytesSerde: KeyValueSerde[org.apache.kafka.common.utils.Bytes] = JSerdes.Bytes().toKeyValueSerde()
+  implicit def floatSerde: KeyValueSerde[Float] = JSerdes.Float().asInstanceOf[Serde[Float]].toKeyValueSerde()
+  implicit def javaFloatSerde: KeyValueSerde[java.lang.Float] = JSerdes.Float().toKeyValueSerde()
+  implicit def doubleSerde: KeyValueSerde[Double] = JSerdes.Double().asInstanceOf[Serde[Double]].toKeyValueSerde()
+  implicit def javaDoubleSerde: KeyValueSerde[java.lang.Double] = JSerdes.Double().toKeyValueSerde()
+  implicit def integerSerde: KeyValueSerde[Int] = JSerdes.Integer().asInstanceOf[Serde[Int]].toKeyValueSerde()
+  implicit def javaIntegerSerde: KeyValueSerde[java.lang.Integer] = JSerdes.Integer().toKeyValueSerde()
+  implicit def booleanSerde: KeyValueSerde[Boolean] = new KeyValueSerde[Boolean] {
+    val intSerde = JSerdes.Integer()
+    override def serializer(): Serializer[Boolean] =
+      (topic: String, data: Boolean) => intSerde.serializer().serialize(topic, if (data) 1 else 0)
+    override def deserializer(): Deserializer[Boolean] =
+      (topic: String, data: Array[Byte]) => intSerde.deserializer().deserialize(topic, data) > 0
   }
-  implicit def String: Serde[String] = JSerdes.String()
-  implicit def Long: Serde[Long] = JSerdes.Long().asInstanceOf[Serde[Long]]
-  implicit def JavaLong: Serde[java.lang.Long] = JSerdes.Long()
-  implicit def ByteArray: Serde[Array[Byte]] = JSerdes.ByteArray()
-  implicit def Bytes: Serde[org.apache.kafka.common.utils.Bytes] = JSerdes.Bytes()
-  implicit def Float: Serde[Float] = JSerdes.Float().asInstanceOf[Serde[Float]]
-  implicit def JavaFloat: Serde[java.lang.Float] = JSerdes.Float()
-  implicit def Double: Serde[Double] = JSerdes.Double().asInstanceOf[Serde[Double]]
-  implicit def JavaDouble: Serde[java.lang.Double] = JSerdes.Double()
-  implicit def Integer: Serde[Int] = JSerdes.Integer().asInstanceOf[Serde[Int]]
-  implicit def JavaInteger: Serde[java.lang.Integer] = JSerdes.Integer()
 
   implicit def groupedFromSerde[K, V](
     implicit keySerde: KeySerde[K],
@@ -57,7 +58,19 @@ trait StreamsImplicits {
     otherValueSerde: ValueSerde[VO]
   ): Joined[K, V, VO] =
     Joined.`with`[K, V, VO]
+
+  def materializeAs[K, V](name: String)(
+    implicit keySerde: KeySerde[K],
+    valueSerde: ValueSerde[V]
+  ): Materialized[K, V, KeyValueStore[Bytes, Array[Byte]]] =
+    Materialized.as[K, V, KeyValueStore[Bytes, Array[Byte]]](name)
 }
 object StreamsImplicits {
   trait KeyValueSerde[T] extends KeySerde[T] with ValueSerde[T]
+  implicit class RichSerde[T](serde: Serde[T]) {
+    def toKeyValueSerde(): KeyValueSerde[T] = new KeyValueSerde[T] {
+      override def serializer(): Serializer[T] = serde.serializer()
+      override def deserializer(): Deserializer[T] = serde.deserializer()
+    }
+  }
 }
